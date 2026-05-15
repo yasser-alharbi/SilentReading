9c612db769753ee66ac31809fc798700ed5ab620
Yasser Alharbi <120180262+yasser-alharbi@users.noreply.github.com>
2026-05-15 22:14:40 +0300

    Update optim_new.py

--- optim_new.py
+++ optim_new.py
@@ -1,11 +1,11 @@
 import torch
 from torch.optim import AdamW
 
-def build_optimizer(args, model, mode="cet-mae"):
+def build_optimizer(args, model, mode="srcp"):
     parameters = [p for p in model.parameters() if p.requires_grad]
 
-    if mode == "cet-mae":
-        lr = float(args.get('cet_mae_lr', 5e-7))
+    if mode == "srcp":
+        lr = float(args.get('srcp_lr', 5e-7))
         weight_decay = float(args.get('weight_decay', 1e-2))
         beta1 = float(args.get('adam_beta1', 0.9))
         beta2 = float(args.get('adam_beta2', 0.999))
