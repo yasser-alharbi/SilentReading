eada09806635795ff3c226e91ff3d4c0b12f1286
Yasser Alharbi <120180262+yasser-alharbi@users.noreply.github.com>
2026-05-15 21:42:34 +0300

    Add files via upload

---
+++ contrastive_eeg_pretraining/pre_encoder.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+
+class SRCPContrastiveModel(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+
+    def forward(self, *args, **kwargs):
+        pass
